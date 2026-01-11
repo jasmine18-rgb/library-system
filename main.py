d915f22444e8529a736d850f780fa9bc4f46d7a9
jasmine18-rgb <jasminegithub@gmail.com>
2026-01-11 20:13:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def view_books():
 # RUN APP
 # ==============================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
 
 
